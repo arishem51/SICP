e7426460ac7d311dd75bbbb12b49b270aa7d697c
Arishemm <arishem51@gmail.com>
2023-06-27 10:32:49 +0700

    chore: exercise 111

--- AbstractionWithProcedures/ProcessesAndProcedures/index.js
+++ AbstractionWithProcedures/ProcessesAndProcedures/index.js
@@ -2,6 +2,8 @@
 
 const ZERO = 0;
 const ONE = 1;
+const TWO = 2;
+const THREE = 3;
 
 const createIsEqualToNumber = (numberToEqual) => {
   return (numberToCompare) => {
@@ -34,7 +36,7 @@ function factorialV2(n) {
 
 function generateProductNum(product, counter, maxCount) {
   if (counter <= maxCount) {
-    return generateProductNum(product * counter, counter + 1, maxCount);
+    return generateProductNum(product * counter, counter + ONE, maxCount);
   }
   return product;
 }
@@ -50,7 +52,7 @@ function fibonacci(num) {
   if (isZero(num) || isOne(num)) {
     return num;
   }
-  return fibonacci(reduceOne(num)) + fibonacci(num - 2);
+  return fibonacci(reduceOne(num)) + fibonacci(num - TWO);
 }
 
 // 4 => fib(3) + fib(2) => fib(1) + fib(2) + fib(1) + fib(0) + 1 + 1 + 0 + 1 + 0
@@ -69,3 +71,25 @@ function fibIter(first, second, count) {
 // 4 => fib(1,0,4) => fib(1,1,3) => fib(2, 1,2) => fib(3,2,1) => fib(5,3,0)
 
 console.log(fibonacci(4), fibonacciV2(4));
+
+// f(n) = n if n < 3 and f(n) = f(n – 1) + 2f(n – 2) + 3f(n – 3) if n >= 3
+
+// f(4) = f(3) + 2f(2) + 3f(1) = f(2) + 2f(1) + 3f(0) + 4 + 3 = 2 + 2 + 0 + 4 + 3 = 11
+
+function exercise111(number) {
+  if (number < THREE) {
+    return number;
+  }
+  return (
+    exercise111(reduceOne(number)) +
+    TWO * exercise111(number - TWO) +
+    THREE * exercise111(number - THREE)
+  );
+}
+
+function exercise111V2(number) {
+  // V2 content
+  return number;
+}
+
+console.log(exercise111(4), exercise111V2(4));
