2ae3a000fa6edf97dc2eb51cfb8a26a0eec754f5
Arishemm <arishem51@gmail.com>
2023-07-05 15:52:01 +0700

    chore: naming and the environment

---
+++ AbstractionWithProcedures(Functions)/ElementOfProgramming/index.js
@@ -0,0 +1,25 @@
+// Expressions
+
+137 + 349;
+
+const size = 2;
+
+console.log(size);
+
+console.log(size * 5);
+
+const pi = 3.14159;
+const radius = 10;
+
+console.log(pi * radius * radius);
+
+const circumference = 2 * pi * radius;
+
+console.log(circumference);
+
+/* 
+    * The simples means of abstraction is Constant declaration. 
+
+    * It allows us to use simple names to refer to the results of compound operations (such
+as the circumference computed above).
+ */
