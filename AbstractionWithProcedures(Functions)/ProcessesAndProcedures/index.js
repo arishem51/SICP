68eeb7d23072c667908357f7695fd0ab57fa3e3b
Arishemm <arishem51@gmail.com>
2023-07-06 13:50:24 +0700

    feat: SICP

--- AbstractionWithProcedures(Functions)/ProcessesAndProcedures/index.js
+++ AbstractionWithProcedures(Functions)/ProcessesAndProcedures/index.js
@@ -112,3 +112,121 @@ function sumIterative(number) {
 }
 
 console.log(sumIterative(3));
+
+// Example: Counting Change
+
+function countChange(amount) {
+  return cc(amount, 5);
+}
+
+function cc(amount, kindsOfCoins) {
+  if (amount === 0) {
+    return 1;
+  }
+  if (amount < 0 || kindsOfCoins === 0) {
+    return 0;
+  }
+  return (
+    cc(amount, kindsOfCoins - 1) +
+    cc(amount - firstDenomination(kindsOfCoins), kindsOfCoins)
+  );
+}
+
+function firstDenomination(kindsOfCoins) {
+  if (kindsOfCoins === 1) {
+    return 1;
+  }
+  if (kindsOfCoins === 2) {
+    return 5;
+  }
+  if (kindsOfCoins === 3) {
+    return 10;
+  }
+  if (kindsOfCoins === 4) {
+    return 20;
+  }
+  if (kindsOfCoins === 5) {
+    return 25;
+  }
+
+  return 0;
+}
+
+console.log(countChange(100));
+
+// Exponentiation
+
+// b = 2, n = 5 => 2 * 2^0 * 2 ^ 1 * 2 ^ 2 * ... 2 ^5
+
+function expt(b, n) {
+  return n === 0 ? 1 : b * expt(b, n - 1);
+}
+
+function exptV2(b, n) {
+  function iterative(total, counter) {
+    if (counter === 0) {
+      return total;
+    }
+    return iterative(total * b, counter - 1);
+  }
+
+  return iterative(1, n);
+}
+
+console.log(expt(2, 3), exptV2(2, 3));
+
+function fastExpt(b, n) {
+  if (n === 0) {
+    return 1;
+  }
+  if (isEven(n)) {
+    return square(fastExpt(b, n / 2));
+  }
+  return b * fastExpt(b, n - 1);
+}
+
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+function square(n) {
+  return n * n;
+}
+
+console.log(fastExpt(2, 3));
+
+// Greatest Common Divisors
+
+function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+}
+
+console.log(gcd(16, 32));
+
+function smallestDivisor(n) {
+  function findDivisor(n, testDivisor) {
+    if (square(testDivisor) > n) {
+      return n;
+    }
+    if (divides(testDivisor, n)) {
+      return testDivisor;
+    }
+    return findDivisor(n, testDivisor + 1);
+  }
+
+  function divides(a, b) {
+    return b % a === 0;
+  }
+
+  return findDivisor(n, 2);
+}
+
+function isPrime(n) {
+  return n === smallestDivisor(n);
+}
+
+console.log(isPrime(16));
